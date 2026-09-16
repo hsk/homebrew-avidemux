1cc7e18950d7786cbaa22174e2b6e3f2a5ad34d5
Hiroshi Sakurai <sakurai777@gmail.com>
2026-09-16 17:23:56 +0900

    Add avidemux2-arm64 cask (self-built, native arm64)

---
+++ Casks/avidemux2-arm64.rb
@@ -0,0 +1,16 @@
+cask "avidemux2-arm64" do
+  version "2.8.2"
+  sha256 "3c102b6aa662a972472b9967ec4db125cfe97e2a8916283a8270438920185ec4"
+
+  url "file:///Volumes/AvidemuxBuild/avidemux2/installer/Avidemux 2.8.2 Qt6 260916-17h07m_eb2b7472721.dmg"
+  name "Avidemux (arm64, self-built)"
+  desc "Video editor, built natively for Apple Silicon from source (mean00/avidemux2)"
+  homepage "https://github.com/mean00/avidemux2"
+
+  app "Avidemux-2.8.2.app"
+
+  zap trash: [
+    "~/Library/Preferences/org.avidemux.avidemux2.plist",
+    "~/Library/Application Support/Avidemux",
+  ]
+end
